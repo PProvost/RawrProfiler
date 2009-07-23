e9c830e8e3f91a4c10271b71d56b7cb5242d4f62
PProvost <peter@provost.org>
2009-07-23 16:47:35 -0600

    Coded equipment, talents, bags and bank

--- RawrProfiler.lua
+++ RawrProfiler.lua
@@ -18,28 +18,27 @@ local L = setmetatable({
 	-- ["Foo"] = "This is foo",
 }, {__index=function(t,i) return i end})
 
-local defaults = {
-	inventory = {},
-}
 local db
-local function Print(...) print("|cFF33FF99Rawr Profiler|r:", ...) end
+local realm, player = GetRealmName(), GetUnitName("player", false)
+local function Print(...) print("|cFF33FF99RawrProfiler|r:", ...) end
 local debugf = tekDebug and tekDebug:GetFrame("RawrProfiler")
-local function Debug(...) if debugf then debugRawrProfiler:AddMessage(string.join(", ", tostringall(...))) end end
+local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
 
 --[[ Addon Declaration ]]
-local RawrProfiler = CreateFrame("frame")
+RawrProfiler = CreateFrame("frame")
 RawrProfiler:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
 RawrProfiler:RegisterEvent("ADDON_LOADED")
 
 function RawrProfiler:ADDON_LOADED(event, addon)
 	if addon:lower() ~= "rawrprofiler" then return end
 
-	RawrProfilerDB = setmetatable(RawrProfilerDB or {}, {__index = defaults})
-	db = RawrProfilerDB
-
 	-- Do anything you need to do after addon has loaded
+	self:RegisterEvent("UNIT_INVENTORY_CHANGED")
+	self:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+	self:RegisterEvent("PLAYER_TALENT_UPDATE")
+	self:RegisterEvent("BANKFRAME_OPENED")
 
-	LibStub("tekKonfig-AboutPanel").new("RawrProfiler", "RawrProfiler") -- Make first arg nil if no parent config panel
+	LibStub("tekKonfig-AboutPanel").new(nil, "RawrProfiler") -- Make first arg nil if no parent config panel
 
 	self:UnregisterEvent("ADDON_LOADED")
 	self.ADDON_LOADED = nil
@@ -47,33 +46,115 @@ function RawrProfiler:ADDON_LOADED(event, addon)
 	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
 end
 
+function RawrProfiler:UNIT_INVENTORY_CHANGED()
+	self:ScanEquippedItems()
+	self:ScanInventory()
+end
 
 function RawrProfiler:PLAYER_LOGIN()
-	self:RegisterEvent("PLAYER_LOGOUT")
-
 	-- Do anything you need to do after the player has entered the world
-	self:ScanAll()
+	realm, player = GetRealmName(), GetUnitName("player", false)
+	self:CheckDB()
+
+	-- Delay the scan until GetTalentTabInfo returns data
+	-- because for some reason Talent info isnt available on PLAYER_LOGIN
+	local total = 0
+	self:SetScript("OnUpdate", function(self, elapsed)
+		total = total + elapsed
+		if total >= 1.0 then -- try every half second
+			Debug("Checking whether talents are available")
+			if db and GetTalentTabInfo(1) then
+				Debug("Good to go", db)
+				self:ScanAll()
+				self:SetScript("OnUpdate", nil)
+			end
+		end
+	end)
 
 	self:UnregisterEvent("PLAYER_LOGIN")
 	self.PLAYER_LOGIN = nil
 end
 
+function RawrProfiler:ACTIVE_TALENT_GROUP_CHANGED()
+	Debug("Talent change detected.")
+	if db then self:ScanTalents() end
+end
+RawrProfiler.PLAYER_TALENT_UPDATE = RawrProfiler.ACTIVE_TALENT_GROUP_CHANGED
 
-function RawrProfiler:PLAYER_LOGOUT()
-	for i,v in pairs(defaults) do if db[i] == v then db[i] = nil end end
-	-- Do anything you need to do as the player logs out
+function RawrProfiler:BANKFRAME_OPENED()
+	self:ScanBank()
+end
+
+function RawrProfiler:CheckDB()
+	if not RawrProfilerDB then RawrProfilerDB = {} end
+	if not RawrProfilerDB[realm] then RawrProfilerDB[realm] = {} end
+	if not RawrProfilerDB[realm][player] then 
+		RawrProfilerDB[realm][player] = {
+			equipped = {},
+			inventory = {},
+			bank = {},
+			glyphs = {},
+		}
+	end
+	db = RawrProfilerDB[realm][player]
+	db.patch = GetBuildInfo() -- What patch level was this taken?
 end
 
 function RawrProfiler:ScanAll()
+	self:ScanEquippedItems()
 	self:ScanInventory()
+	self:ScanTalents()
 end
 
---[[ Scans currently equipped items ]]
-function RawrProfiler:ScanInventory()
+function RawrProfiler:ScanEquippedItems()
 	local slots = { "AmmoSlot", "BackSlot", "Bag0Slot", "Bag1Slot", "Bag2Slot", "Bag3Slot", "ChestSlot", "FeetSlot", "Finger0Slot", "Finger1Slot", "HandsSlot", "HeadSlot", "LegsSlot", "MainHandSlot", "NeckSlot", "RangedSlot", "SecondaryHandSlot", "ShirtSlot", "ShoulderSlot", "TabardSlot", "Trinket0Slot", "Trinket1Slot", "WaistSlot", "WristSlot" }
-	for i, slot in ipairs(slots) do
+	Debug("Scanning Equipped Items")
+	for i, slotName in ipairs(slots) do
+		local slot = GetInventorySlotInfo(slotName)
 		local link = GetInventoryItemLink("player", slot)
-		db.inventory[slot] = link
+		db.equipped[slotName] = link
 	end
+end
+
+local function ScanBag(bag, dbtable)
+	Debug("ScanBag", bag, GetContainerNumSlots(bag))
+	for slot = 1, GetContainerNumSlots(bag) do
+		local link = GetContainerItemLink(bag, slot)
+		if link then
+			local class = select(6, GetItemInfo(link))
+			if class=="Weapon" or class=="Armor" or class=="Quiver" or class=="Projectile" then
+				table.insert(dbtable, link)
+			end
+		end
+	end
+end
+
+function RawrProfiler:ScanInventory()
+	Debug("Scanning Bags")
+	db.inventory = {}
+	for bag = 0, NUM_BAG_SLOTS do
+		ScanBag(bag, db.inventory)
+	end
+end
+
+function RawrProfiler:ScanBank()
+	Debug("Scanning Bank")
+	db.bank = {}
+	ScanBag(BANK_CONTAINER, db.bank)
+	for bag = NUM_BAG_SLOTS+1, NUM_BAG_SLOTS+NUM_BANKBAGSLOTS do
+		ScanBag(bag, db.bank)
+	end
+end
+
+function RawrProfiler:ScanTalents()
+	Debug("Scanning Talents")
+	local talentString = ""
+	for tabIndex = 1,GetNumTalentTabs("player") do
+		for talentIndex = 1,GetNumTalents(tabIndex, false, false) do
+			local points = select(5, GetTalentInfo(tabIndex, talentIndex, false, false, nil))
+			talentString = talentString .. tostring(points)
+		end
+	end
+	db.talents = talentString
 end
 
